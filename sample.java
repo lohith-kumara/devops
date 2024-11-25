09eb662850cd43202470d52c86ea9e203c1f543f
lohith-kumara <appulohith10@gmail.com>
2024-11-25 08:43:38 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -3,6 +3,8 @@ class Sample{
    static public void main(String []args){
 
      System.out.println("Welcome to Sharavesh Tech");
-      System.out.println("Welcome to Sharavesh Tech");
+      
+      System.out.println("Welcome to Sharavesh Tech")
+         System.out.println("Welcome to Sharavesh Tech");
  }
 }
